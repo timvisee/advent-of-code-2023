d1a4b7d149390ad753bbe9b95d691b2202d1c701
timvisee <tim@visee.me>
2023-12-03 19:56:42 +0100

    Add day02b

--- day02b/src/main.rs
+++ day02b/src/main.rs
@@ -5,14 +5,13 @@ pub fn main() {
         "{}",
         include_bytes!("../input.txt")
             .split(|b| b == &b'\n')
-            .enumerate()
-            .filter_map(|(game_id, line)| {
+            .map(|line| {
                 let mut rgb = [0, 0, 0];
                 line.splitn(2, |b| b == &b':')
                     .nth(1)
                     .unwrap()
                     .split(|b| b == &b',' || b == &b';')
-                    .all(|item| {
+                    .for_each(|item| {
                         let i = match item[1..].splitn(2, |b| *b == b' ').nth(1).unwrap() {
                             b"red" => 0usize,
                             b"green" => 1,
@@ -20,10 +19,9 @@ pub fn main() {
                             _ => unreachable!(),
                         };
                         rgb[i] = rgb[i].max(atoi(&item[1..]).unwrap());
-                        rgb[i] <= 12 + i as u32
-                    })
-                    .then_some(game_id + 1)
+                    });
+                rgb.iter().product::<u32>()
             })
-            .sum::<usize>()
+            .sum::<u32>()
     );
 }
