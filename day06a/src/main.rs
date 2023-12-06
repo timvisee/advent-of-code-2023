caee82aabe1965ee00cb735de56fe8bd2016ac96
timvisee <tim@visee.me>
2023-12-06 09:46:36 +0100

    Optimize day06a with quadratic function

--- day06a/src/main.rs
+++ day06a/src/main.rs
@@ -6,17 +6,17 @@ pub fn main() {
         "{}",
         input[11..newline]
             .split(|b| b == &b' ')
-            .flat_map(atoi::atoi::<u64>)
+            .flat_map(atoi::atoi::<usize>)
             .zip(
-                input[newline + 1..]
+                input[newline + 12..]
                     .split(|b| b == &b' ')
-                    .flat_map(atoi::atoi::<u64>)
+                    .flat_map(atoi::atoi::<usize>),
             )
-            .map(|(time, dist)| {
-                (2..time)
-                    .map(|hold| (time - hold) * hold)
-                    .filter(|travel| *travel > dist)
-                    .count()
+            .map(|(t, d)| {
+                // Modified quadratic formula: <https://en.wikipedia.org/wiki/Quadratic_formula>
+                let a = (t - f32::sqrt((t * t - 4 * d) as f32) as usize) / 2;
+                let b = t - a;
+                b - (b * (t - b) <= d) as usize - a - (a * (t - a) <= d) as usize + 1
             })
             .product::<usize>()
     );
