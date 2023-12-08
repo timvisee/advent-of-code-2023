03049d1475fdaf36971d0995ec821477b081eb4e
timvisee <tim@visee.me>
2023-12-08 09:18:08 +0100

    Optimize day08, remove tuple, use single u64

--- README.md
+++ README.md
@@ -27,11 +27,11 @@ scientifically) in [`bench.rs`](./runner/src/bin/bench.rs) on an
 | [day 5](https://adventofcode.com/2023/day/5)   | [` 0.015 ms`](./day05a/src/main.rs) | [` 0.034 ms`](./day05b/src/main.rs) |
 | [day 6](https://adventofcode.com/2023/day/6)   | [` 0.001 ms`](./day06a/src/main.rs) | [` 0.001 ms`](./day06b/src/main.rs) |
 | [day 7](https://adventofcode.com/2023/day/7)   | [` 0.065 ms`](./day07a/src/main.rs) | [` 0.066 ms`](./day07b/src/main.rs) |
-| [day 8](https://adventofcode.com/2023/day/8)   | [` 0.073 ms`](./day08a/src/main.rs) | [` 0.225 ms`](./day08b/src/main.rs) |
+| [day 8](https://adventofcode.com/2023/day/8)   | [` 0.064 ms`](./day08a/src/main.rs) | [` 0.177 ms`](./day08b/src/main.rs) |
 
 |              | one-by-one (1 CPU core)                  | parallel                                     |
 |:-------------|:-----------------------------------------|:---------------------------------------------|
-| _everything_ | [` 0.68 ms`](./runner/src/bin/runner.rs) | [` 0.79 ms`](./runner/src/bin/runner-par.rs) |
+| _everything_ | [` 0.61 ms`](./runner/src/bin/runner.rs) | [` 0.79 ms`](./runner/src/bin/runner-par.rs) |
 
 ## Run solutions
 
--- day08a/src/main.rs
+++ day08a/src/main.rs
@@ -2,12 +2,12 @@ pub fn main() {
     let input = include_bytes!("../input.txt");
     let split = input.iter().position(|&c| c == b'\n').unwrap();
 
-    let mut map = [(0, 0); 174763];
+    let mut map = [0u64; 174763];
     let enc = |n: &[u8]| {
-        ((n[0] - b'0') as u32) << 12 | ((n[1] - b'0') as u32) << 6 | (n[2] - b'0') as u32
+        ((n[0] - b'0') as u64) << 12 | ((n[1] - b'0') as u64) << 6 | (n[2] - b'0') as u64
     };
     input[split + 2..].split(|&c| c == b'\n').for_each(|node| {
-        map[enc(&node[0..3]) as usize] = (enc(&node[7..10]), enc(&node[12..15]));
+        map[enc(&node[0..3]) as usize] = enc(&node[7..10]) | enc(&node[12..15]) << 32;
     });
 
     println!(
@@ -16,9 +16,12 @@ pub fn main() {
             .iter()
             .cycle()
             .scan(enc(b"AAA"), |node, step| {
-                let (l, r) = map[*node as usize];
-                *node = if step == &b'L' { l } else { r };
-                Some(*node & 0b111111 == (b'Z' - b'0') as u32)
+                *node = if step == &b'L' {
+                    map[*node as usize] & u32::MAX as u64
+                } else {
+                    map[*node as usize] >> 32
+                };
+                Some(*node & 0b111111 == (b'Z' - b'0') as u64)
             })
             .position(|node| node)
             .unwrap()
