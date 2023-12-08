c1cd4b63651f50f71ff0e2d10262e6cdc2f5b70b
timvisee <tim@visee.me>
2023-12-08 09:08:01 +0100

    Optimize day08, encode to binary, use lookup map on array

--- day08a/src/main.rs
+++ day08a/src/main.rs
@@ -1,31 +1,24 @@
-use hashbrown::HashMap;
-
 pub fn main() {
     let input = include_bytes!("../input.txt");
     let split = input.iter().position(|&c| c == b'\n').unwrap();
 
-    let map = input[split + 2..]
-        .split(|&c| c == b'\n')
-        .map(|node| {
-            (
-                node[0..3].try_into().unwrap(),
-                (
-                    node[7..10].try_into().unwrap(),
-                    node[12..15].try_into().unwrap(),
-                ),
-            )
-        })
-        .collect::<HashMap<[_; 3], ([_; 3], [_; 3])>>();
+    let mut map = [(0, 0); 174763];
+    let enc = |n: &[u8]| {
+        ((n[0] - b'0') as u32) << 12 | ((n[1] - b'0') as u32) << 6 | (n[2] - b'0') as u32
+    };
+    input[split + 2..].split(|&c| c == b'\n').for_each(|node| {
+        map[enc(&node[0..3]) as usize] = (enc(&node[7..10]), enc(&node[12..15]));
+    });
 
     println!(
         "{}",
         input[0..split]
             .iter()
             .cycle()
-            .scan(*b"AAA", |node, step| {
-                let (l, r) = map[node];
+            .scan(enc(b"AAA"), |node, step| {
+                let (l, r) = map[*node as usize];
                 *node = if step == &b'L' { l } else { r };
-                Some(*node == *b"ZZZ")
+                Some(*node & 63 == (b'Z' - b'0') as u32)
             })
             .position(|node| node)
             .unwrap()
--- day08b/src/main.rs
+++ day08b/src/main.rs
@@ -1,35 +1,30 @@
-use hashbrown::HashMap;
-
 pub fn main() {
     let input = include_bytes!("../input.txt");
     let split = input.iter().position(|&c| c == b'\n').unwrap();
 
-    let map = input[split + 2..]
-        .split(|&c| c == b'\n')
-        .map(|node| {
-            (
-                node[0..3].try_into().unwrap(),
-                (
-                    node[7..10].try_into().unwrap(),
-                    node[12..15].try_into().unwrap(),
-                ),
-            )
-        })
-        .collect::<HashMap<[_; 3], ([_; 3], [_; 3])>>();
+    let (mut map, mut starts) = ([(0, 0); 174763], Vec::with_capacity(6));
+    let enc = |n: &[u8]| {
+        ((n[0] - b'0') as u32) << 12 | ((n[1] - b'0') as u32) << 6 | (n[2] - b'0') as u32
+    };
+    input[split + 2..].split(|&c| c == b'\n').for_each(|node| {
+        map[enc(&node[0..3]) as usize] = (enc(&node[7..10]), enc(&node[12..15]));
+        if node[2] == b'A' {
+            starts.push(enc(&node[0..3]));
+        }
+    });
 
     println!(
         "{}",
-        map.keys()
-            .filter(|node| node[2] == b'Z')
-            .cloned()
+        starts
+            .into_iter()
             .map(|node| {
                 input[0..split]
                     .iter()
                     .cycle()
                     .scan(node, |node, step| {
-                        let (l, r) = map[node];
+                        let (l, r) = map[*node as usize];
                         *node = if step == &b'L' { l } else { r };
-                        Some(node[2] == b'Z')
+                        Some(*node & 63 == (b'Z' - b'0') as u32)
                     })
                     .position(|node| node)
                     .unwrap()
