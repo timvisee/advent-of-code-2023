da641551c007df24de8da3030995e5e4a1d6581b
timvisee <tim@visee.me>
2023-12-09 10:17:22 +0100

    Optimize day08, bitpacking into 15 bits

--- day08a/src/main.rs
+++ day08a/src/main.rs
@@ -2,12 +2,12 @@ pub fn main() {
     let input = include_bytes!("../input.txt");
     let split = input.iter().position(|&c| c == b'\n').unwrap();
 
-    let mut map = [0u64; 174763];
+    let mut map = [0u32; 0b11001_11001_11001 + 1];
     let enc = |n: &[u8]| {
-        ((n[0] - b'0') as u64) << 12 | ((n[1] - b'0') as u64) << 6 | (n[2] - b'0') as u64
+        ((n[0] - b'A') as u32) << 10 | ((n[1] - b'A') as u32) << 5 | (n[2] - b'A') as u32
     };
     input[split + 2..].split(|&c| c == b'\n').for_each(|node| {
-        map[enc(&node[0..3]) as usize] = enc(&node[7..10]) | enc(&node[12..15]) << 32;
+        map[enc(&node[0..3]) as usize] = enc(&node[7..10]) | enc(&node[12..15]) << 16;
     });
 
     println!(
@@ -17,11 +17,11 @@ pub fn main() {
             .cycle()
             .scan(enc(b"AAA"), |node, step| {
                 *node = if step == &b'L' {
-                    map[*node as usize] & u32::MAX as u64
+                    map[*node as usize] & u16::MAX as u32
                 } else {
-                    map[*node as usize] >> 32
+                    map[*node as usize] >> 16
                 };
-                Some(*node & 0b111111 == (b'Z' - b'0') as u64)
+                Some(*node & 0b11111 == (b'Z' - b'A') as u32)
             })
             .position(|node| node)
             .unwrap()
