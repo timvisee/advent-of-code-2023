0c9ae6322b01868723d0b454536edd6670cbc092
timvisee <tim@visee.me>
2023-12-09 10:26:18 +0100

    Update brute force solution for day08

--- day08b/src/bin/brute_force.rs
+++ day08b/src/bin/brute_force.rs
@@ -5,12 +5,12 @@ pub fn main() {
     let input = include_bytes!("../../input.txt");
     let split = input.iter().position(|&c| c == b'\n').unwrap();
 
-    let (mut map, mut nodes) = ([0u64; 174763], Vec::with_capacity(6));
+    let (mut map, mut nodes) = ([0u32; 0b11001_11001_11001 + 1], Vec::with_capacity(6));
     let enc = |n: &[u8]| {
-        ((n[0] - b'0') as u64) << 12 | ((n[1] - b'0') as u64) << 6 | (n[2] - b'0') as u64
+        ((n[0] - b'A') as u32) << 10 | ((n[1] - b'A') as u32) << 5 | (n[2] - b'A') as u32
     };
     input[split + 2..].split(|&c| c == b'\n').for_each(|node| {
-        map[enc(&node[0..3]) as usize] = enc(&node[7..10]) | enc(&node[12..15]) << 32;
+        map[enc(&node[0..3]) as usize] = enc(&node[7..10]) | enc(&node[12..15]) << 16;
         if node[2] == b'A' {
             nodes.push(enc(&node[0..3]));
         }
@@ -24,14 +24,14 @@ pub fn main() {
         .map(|step| {
             for node in &mut nodes {
                 *node = if step == &b'L' {
-                    map[*node as usize] & u32::MAX as u64
+                    map[*node as usize] & u16::MAX as u32
                 } else {
-                    map[*node as usize] >> 32
+                    map[*node as usize] >> 16
                 };
             }
             nodes
                 .iter()
-                .all(|node| *node & 0b111111 == (b'Z' - b'0') as u64)
+                .all(|node| *node & 0b111111 == (b'Z' - b'A') as u32)
         })
         .take(RUNS)
         .position(|node| node);
