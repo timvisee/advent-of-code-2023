c6074d70e166372d572e3d44fffca370dafbae24
timvisee <tim@visee.me>
2023-12-09 12:04:39 +0100

    Add day09

---
+++ day09a/src/main.rs
@@ -0,0 +1,26 @@
+use lending_iterator::LendingIterator;
+
+pub fn main() {
+    println!(
+        "{}",
+        include_bytes!("../input.txt")
+            .split(|b| b == &b'\n')
+            .map(|line| {
+                let mut nums = line
+                    .split(|b| b == &b' ')
+                    .map(|b| atoi::atoi::<i32>(b).unwrap())
+                    .collect::<Vec<_>>();
+
+                (0..nums.len())
+                    .rev()
+                    .map(|len| {
+                        lending_iterator::windows_mut(&mut nums[..=len])
+                            .for_each(|w: &mut [_; 2]| w[0] = w[1] - w[0]);
+                        nums[len]
+                    })
+                    .take_while(|n| *n != 0)
+                    .sum::<i32>()
+            })
+            .sum::<i32>(),
+    );
+}
