c50b8701aafa509af13b19c1cd65a4dcb22355af
timvisee <tim@visee.me>
2023-12-10 14:18:28 +0100

    Add day10

--- README.md
+++ README.md
@@ -29,10 +29,11 @@ scientifically) in [`bench.rs`](./runner/src/bin/bench.rs) on an
 | [day 7](https://adventofcode.com/2023/day/7)   | [` 0.065 ms`](./day07a/src/main.rs) | [` 0.066 ms`](./day07b/src/main.rs) |
 | [day 8](https://adventofcode.com/2023/day/8)   | [` 0.028 ms`](./day08a/src/main.rs) | [` 0.122 ms`](./day08b/src/main.rs) |
 | [day 9](https://adventofcode.com/2023/day/9)   | [` 0.042 ms`](./day09a/src/main.rs) | [` 0.042 ms`](./day09b/src/main.rs) |
+| [day 10](https://adventofcode.com/2023/day/10) | [` 0.048 ms`](./day10a/src/main.rs) | [` 0.095 ms`](./day10b/src/main.rs) |
 
 |              | one-by-one (1 CPU core)                  | parallel                                     |
 |:-------------|:-----------------------------------------|:---------------------------------------------|
-| _everything_ | [` 0.59 ms`](./runner/src/bin/runner.rs) | [` 0.31 ms`](./runner/src/bin/runner-par.rs) |
+| _everything_ | [` 0.73 ms`](./runner/src/bin/runner.rs) | [` 0.31 ms`](./runner/src/bin/runner-par.rs) |
 
 ## Run solutions
 
---
+++ day10b/src/main.rs
@@ -0,0 +1,61 @@
+pub fn main() {
+    let map = include_bytes!("../input.txt");
+    let width = map.iter().position(|&b| b == b'\n').unwrap();
+    let start = map.iter().position(|&b| b == b'S').unwrap();
+
+    let mut covered = vec![false; (width + 1) * width];
+    let (mut pos, mut dir) = {
+        if matches!(map[start - width - 1], b'|' | b'7' | b'F') {
+            (start - width - 1, 0)
+        } else if matches!(map[start + width + 1], b'|' | b'L' | b'J') {
+            (start + width + 1, 2)
+        } else {
+            (start - 1, 3)
+        }
+    };
+
+    std::iter::repeat(())
+        .position(|_| unsafe {
+            *covered.get_unchecked_mut(pos) = true;
+            match (map.get_unchecked(pos), dir) {
+                (b'|', 2) => pos += width + 1,
+                (b'|', 0) => pos -= width + 1,
+                (b'-', 3) => pos -= 1,
+                (b'-', 1) => pos += 1,
+                (b'L', 2) | (b'F', 0) => {
+                    pos += 1;
+                    dir = 1;
+                }
+                (b'L', 3) | (b'J', 1) => {
+                    pos -= width + 1;
+                    dir = 0;
+                }
+                (b'7', 0) | (b'J', 2) => {
+                    pos -= 1;
+                    dir = 3;
+                }
+                (b'7', 1) | (b'F', 3) => {
+                    pos += width + 1;
+                    dir = 2;
+                }
+                (b'S', _) => return true,
+                (_, _) => unreachable!(),
+            }
+            false
+        })
+        .unwrap();
+
+    let mut inside = false;
+    println!(
+        "{}",
+        map.iter()
+            .enumerate()
+            .filter(|(pos, cell)| {
+                let pipe = unsafe { *covered.get_unchecked(*pos) };
+                inside &= pos % (width + 1) != 0;
+                inside ^= pipe && matches!(*cell, b'|' | b'F' | b'7');
+                inside && (!pipe || **cell == b'.') && (pos % (width + 1) != width)
+            })
+            .count()
+    );
+}
